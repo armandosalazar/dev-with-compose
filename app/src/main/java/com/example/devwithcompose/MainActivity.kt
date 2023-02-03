bf6d798afdceaaf7771986d96c532c6a9e6bd723
Armando Salazar <armandosalazar351@gmail.com>
2023-02-03 13:43:46 -0600

    Add Column and Row Layout

--- app/src/main/java/com/example/devwithcompose/MainActivity.kt
+++ app/src/main/java/com/example/devwithcompose/MainActivity.kt
@@ -4,8 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
@@ -14,6 +13,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.devwithcompose.ui.theme.DevWithComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -26,7 +26,8 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+//                    Greeting("Android")
+                    DefaultPreview()
                 }
             }
         }
@@ -45,12 +46,34 @@ fun DefaultPreview() {
         // Los hijos se ajustan de forma relativa de acuerdo al padre
         Box(
             modifier = Modifier
-                .fillMaxSize()
-                .background(Color.LightGray),
+                .fillMaxSize(),
             contentAlignment = Alignment.Center
         ) {
             Greeting("Android", modifier = Modifier.align(Alignment.BottomEnd))
             Greeting("Armando")
+            Column(
+                modifier = Modifier
+                    .align(alignment = Alignment.TopCenter)
+                    .background(Color.Magenta)
+                    .fillMaxWidth()
+                    .height(150.dp),
+                verticalArrangement = Arrangement.SpaceEvenly,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(text = "Hola mundo", modifier = Modifier.background(Color.Yellow))
+                Text(text = "Hola mundo", modifier = Modifier.background(Color.LightGray))
+                Text(text = "Hola mundo", modifier = Modifier.background(Color.Cyan))
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .background(Color.Yellow)
+                        .padding(20.dp),
+                    horizontalArrangement = Arrangement.SpaceEvenly
+                ) {
+                    Text(text = "Hola mundo", modifier = Modifier.background(Color.LightGray))
+                    Text(text = "Hola mundo", modifier = Modifier.background(Color.Cyan))
+                }
+            }
         }
     }
 }
