0a39036df1536938d5bd83208b7564ea8130c8a2
Armando Salazar <armandosalazar351@gmail.com>
2023-02-03 13:24:46 -0600

    Add Box Layout

--- app/src/main/java/com/example/devwithcompose/MainActivity.kt
+++ app/src/main/java/com/example/devwithcompose/MainActivity.kt
@@ -3,12 +3,16 @@ package com.example.devwithcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.devwithcompose.ui.theme.DevWithComposeTheme
 
@@ -30,14 +34,23 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(text = "Hello $name!", modifier = modifier)
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, name = "Android Greeting", showSystemUi = true)
 @Composable
 fun DefaultPreview() {
     DevWithComposeTheme {
-        Greeting("Android")
+        // Los hijos se ajustan de forma relativa de acuerdo al padre
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Color.LightGray),
+            contentAlignment = Alignment.Center
+        ) {
+            Greeting("Android", modifier = Modifier.align(Alignment.BottomEnd))
+            Greeting("Armando")
+        }
     }
 }
